c976e141a3ac9ba93d70af26835c0041443775dd
Joemar Valete <112531106+ntt-joemarvalete@users.noreply.github.com>
2022-10-18 15:11:21 +0800

    Update tsEligibility "Account is not RGB" transition to "notRBG"

--- ORBIT_PLDTCares/configurations/components/tsEligibility_config.js
+++ ORBIT_PLDTCares/configurations/components/tsEligibility_config.js
@@ -81,7 +81,7 @@ module.exports = {
         InvalidServiceNumber: { Message: 'Invalid service number', Conversation: { Transition: 'undertreatment', Variables: ['ineligibleAcctmsg'] }},
         WithOpenSO: { Message: 'With Open SO.', Conversation: { Transition: 'openso', Variables: ['ineligibleAcctmsg', 'openSONumber'] }},
         WithOpenTransferSO: { Message: 'With Open Transfer SO.', Conversation: { Transition: 'undertreatment', Variables: ['ineligibleAcctmsg', 'openSONumber'] }},
-        AccountIsNotRBG: { Message: 'Account is not RBG.', Conversation: { Transition: 'undertreatment', Variables: ['ineligibleAcctmsg'] }},
+        AccountIsNotRBG: { Message: 'Account is not RBG.', Conversation: { Transition: 'notRBG', Variables: ['ineligibleAcctmsg'] }},
         OpenOrder: { Conversation: { Transition: 'openorder', Variables: ['ineligibleAcctmsg', 'openSONumber'] }},
     }
 }
